dd25da31aff391cb871f26bc3a0aecf6d9d7b128
Mr-Yellow <mr-yellow@mr-yellow.com>
2019-12-12 11:19:31 +1100

    Removed note on setting up handlers

--- test/setup.js
+++ test/setup.js
@@ -9,7 +9,7 @@ before('Init agent instance', (done) => {
 		.then(() => done());
 });
 
-before('Setup emitter handlers', (done)=> {
+before('Setup emitter handlers', ()=> {
 	agents
 		.on('init', ()=> mlog.log('Created agents interface'))
 		.on('ready', ()=> mlog.log('Agents interface ready'))
@@ -22,8 +22,6 @@ before('Setup emitter handlers', (done)=> {
 		.on('runImmediate', id => mlog.log('Agent', id, 'marked for immediate run!'))
 		.on('log', (session, ...args) => mlog.log(...args))
 		.on('warn', (session, ...args) => mlog.log('Warning', ...args));
-	// FIXME: Is there any event we can wait for?
-	done();
 });
 
 after(()=> agents.destroy());
