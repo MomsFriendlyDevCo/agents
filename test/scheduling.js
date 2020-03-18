6d713caf801f61a0092607ddbdbe38fb702da54e
Mr-Yellow <mr-yellow@mr-yellow.com>
2020-03-18 14:05:00 +1100

    Comment appropriate test

--- test/scheduling.js
+++ test/scheduling.js
@@ -7,13 +7,13 @@ describe('Ensuring scheduling is queued', function() {
 
 	beforeEach(() => agents.invalidate('timed'));
 
-	// FIXME: This may pass when re-ran as the scheduled task is not destroyed when invalidated.
 	it('should have no result immediately after invalidate', ()=>
-		agents.getSize('timed').then((size) =>
-			expect(size).to.be.undefined
-		)
+	agents.getSize('timed').then((size) =>
+	expect(size).to.be.undefined
+	)
 	);
-
+	
+	// FIXME: This may pass when re-ran as the scheduled task is not destroyed when invalidated.
 	it('should have a result once triggered', (done)=> {
 		// Wait until after agent should have ran.
 		setTimeout(()=>{
