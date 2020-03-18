87a45904ba53abd046c0de590269a963510db3c0
Mr-Yellow <mr-yellow@mr-yellow.com>
2020-03-18 14:04:41 +1100

    Separated timing into two tests

--- test/scheduling.js
+++ test/scheduling.js
@@ -8,19 +8,20 @@ describe('Ensuring scheduling is queued', function() {
 	beforeEach(() => agents.invalidate('timed'));
 
 	// FIXME: This may pass when re-ran as the scheduled task is not destroyed when invalidated.
-	it('should have a result once triggered', (done)=> {
-		expect(agents.has('timed')).to.be.true;
-		agents.getSize('timed').then((size) => {
-			expect(size).to.be.undefined;
+	it('should have no result immediately after invalidate', ()=>
+		agents.getSize('timed').then((size) =>
+			expect(size).to.be.undefined
+		)
+	);
 
-			// Wait until after agent should have ran.
-			setTimeout(()=>{
-				agents.getSize('timed').then((size) => {
-					expect(size).to.be.equal(17);
-					done();
-				});
-			}, 7000);
-		});
+	it('should have a result once triggered', (done)=> {
+		// Wait until after agent should have ran.
+		setTimeout(()=>{
+			agents.getSize('timed').then((size) => {
+				expect(size).to.be.equal(17);
+				done();
+			});
+		}, 7000);
 	});
 
 });
